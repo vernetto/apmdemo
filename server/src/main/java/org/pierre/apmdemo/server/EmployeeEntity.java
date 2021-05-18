759e40e575f91fb7db4e7625068bec3d6c7e343a
vernetto <Ernesto0z>
2021-05-18 16:03:28 +0200

    all

--- server/src/main/java/org/pierre/apmdemo/server/EmployeeEntity.java
+++ server/src/main/java/org/pierre/apmdemo/server/EmployeeEntity.java
@@ -2,12 +2,14 @@ package org.pierre.apmdemo.server;
 
 import lombok.Data;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@Cacheable
 public class EmployeeEntity {
     @Id @GeneratedValue
     private Long id;
